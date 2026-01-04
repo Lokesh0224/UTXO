9baea02c1b1324f01910bb631c29cc4bcc6c470f
Lokesh0224 <lokeshk20019@gmail.com>
2026-01-04 23:45:05 +0530

    feat: added the models

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+struct Utxo {//unspent txn output
+    id: String,
+    owner: String,
+    amount: u64,
+}
+
+#[derive(Debug)]
+struct TxInput {//the amt that's going to consumed
+    utxo_id: String,
+}
+
+#[derive(Debug)]
+struct TxOutput {//amt after consumed
+    owner: String,
+    amount: u64,
+}
+
+#[derive(Debug)]
+struct Transaction {
+    inputs: Vec<TxInput>,
+    outputs: Vec<TxOutput>,
+}
+
+#[derive(Debug)]
+struct Ledger {
+    utxos: HashMap<String, Utxo>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("UTXO Ledger starting...");
 }
