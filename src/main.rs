a2bc5754a3b79c3d0365da38518c4d92acb5f38e
Lokesh0224 <lokeshk20019@gmail.com>
2026-01-05 21:11:59 +0530

    implement transaction application and validation

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,90 @@ impl Ledger{//Create a new and empty ledger
             utxos: HashMap::new(),
         }
     }
+
+    fn apply_tx(&mut self, tx: Transaction) -> Result<(), String> {
+        let mut input_sum = 0;
+
+        for input in &tx.inputs {
+            let utxo = self
+                .utxos
+                .get(&input.utxo_id)
+                .ok_or_else(|| format!("UTXO {} does not exist (double spend?)", input.utxo_id))?;
+
+            input_sum += utxo.amount;
+        }
+
+        // 2. Sum all outputs
+        let output_sum: u64 = tx.outputs.iter().map(|o| o.amount).sum();
+
+        // 3. Enforce conservation of value
+        if input_sum != output_sum {
+            return Err(format!(
+                "Input sum ({}) does not match output sum ({})",
+                input_sum, output_sum
+            ));
+        }
+
+        // 4. Remove spent UTXOs
+        for input in tx.inputs {
+            self.utxos.remove(&input.utxo_id);
+        }
+
+        // 5. Add new UTXOs
+        for (index, output) in tx.outputs.into_iter().enumerate() {
+            let utxo_id = format!("utxo_{}", self.utxos.len() + index + 1);
+
+            let new_utxo = Utxo {
+                id: utxo_id.clone(),
+                owner: output.owner,
+                amount: output.amount,
+            };
+
+            self.utxos.insert(utxo_id, new_utxo);
+        }
+
+        Ok(())
+    }
 }
 
 fn main() {
-    let ledger = Ledger::new();
-    println!("UTXO Ledger initialization {:#?}", ledger);
+    let mut ledger = Ledger::new();
+
+    ledger.utxos.insert( //<String, Utxo>
+        "genesis_1".to_string(),
+        Utxo { 
+            id: "genesis_1".to_string(), 
+            owner: "Alice".to_string(), 
+            amount: 50, 
+        },
+    );
+    println!("Initial ledger: \n{:#?}", ledger);
+
+    //Alice sends 30 to Bob, keeps 20 as change
+    let tx = Transaction {
+        inputs: vec![TxInput{
+            utxo_id: "genesis_1".to_string(),
+        }],
+        outputs: vec![
+            TxOutput{
+                owner: "Bob".to_string(), 
+                amount: 30,
+            }, 
+            TxOutput{
+                owner: "Alice".to_string(), 
+                amount: 20,
+            },
+
+        ],
+    };
+
+    match ledger.apply_tx(tx) {
+        Ok(_) => println!("Transaction applied successfully."),
+        Err(e) => println!("Transaction failed: {}", e),
+    }
+
+    println!("Ledger after transaction:\n{:#?}", ledger);
+    
 }
+
+
